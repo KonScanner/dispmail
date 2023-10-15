1f1a6ee60454cef77afe1093396aadf4409aad8c
Konstantinos K <42744293+KonScanner@users.noreply.github.com>
2023-10-15 17:46:10 +0100

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'DispMail'
-copyright = '2021, KonScanner'
-author = 'KonScanner'
+project = "DispMail"
+copyright = "2021, KonScanner"
+author = "KonScanner"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,25 +31,21 @@ release = '0.1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'main.py']
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "main.py"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
